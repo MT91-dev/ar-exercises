8d0cac8bb2942bf95f8b183da92930c026eec2e4
MT91-dev <tariqm1991@gmail.com>
2022-08-22 23:41:49 +0000

    Add stores, query using where for mens/womens store

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,24 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+# Create three new stores with store information provided
+Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# Check that stores were updated (should be 5 now)
+# puts Store.count
+
+# Find stores where mens apparel is set to true and assign to instance variable
+@mens_stores = Store.where(mens_apparel: true)
+
+# Output each store where mens apparel is true and log the store name and annual revenue next to each store name
+@mens_stores.each do |store| puts "#{store.name}, #{store.annual_revenue}"
+end
+
+# Find stores where womens apparel is set to true and annual revenue less than $1M, assign to instance variable
+@womens_stores = Store.where("womens_apparel = true and annual_revenue < 1000000")
+
+# Output each store where womens apparel is true/annual revenue is less than $1M and log the store name and annual revenue next to each store name
+@womens_stores.each do |store| puts "#{store.name}, #{store.annual_revenue}"
+end
