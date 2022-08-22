ae26dc0f85feb8020a7a4c1d894ffea3f324cc3b
MT91-dev <tariqm1991@gmail.com>
2022-08-22 23:22:19 +0000

    Find store 3 and assign to instance variable, then delete and verify deletion

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,10 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+# Find the store with with id 3 and store it into instance variable and destroy it
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+
+# Verify the store count is updated after destroying store3
+puts Store.count
+
