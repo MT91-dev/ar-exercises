d6a81c918af2be691b62fc24461eef43d6cbf2b7
MT91-dev <tariqm1991@gmail.com>
2022-08-22 23:47:31 +0000

    Calculate total, average and stores with annual revenue >= M dollars

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,14 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# Output the total sum of annual revenues for each store
+total_revenue = Store.sum(:annual_revenue)
+puts total_revenue
+
+# Output the average sum of annual revenues for each store
+average_revenue = Store.average(:annual_revenue)
+puts average_revenue
+
+# Output the best stores that have annual revenue over $1M dollars
+big_revenue_stores = Store.where('annual_revenue >= 1000000').count
+puts big_revenue_stores
