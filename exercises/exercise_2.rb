d71f4632fa6f98a5bbc7dd22fb29b49278b00224
MT91-dev <tariqm1991@gmail.com>
2022-08-22 23:18:53 +0000

    Assign instance variables with store instances by id

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Assign stores to 1 and 2 by the id's
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+# Update the name of store1 with "Greatest"
+@store1.update(name: 'Greatest')
+
+# Log the puts.inspect of store1 to confirm name change
+p @store1
