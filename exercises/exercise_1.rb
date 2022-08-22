62c64e0e566e9ad1e727cfe493ab5b110bdd7c83
MT91-dev <tariqm1991@gmail.com>
2022-08-22 23:17:16 +0000

    Create instances of the store class with data for 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,10 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# Can make separate instances of the class store with the respective store names and fill it with details for that given store
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+# log the count of the stores created so far (should say 3)
+puts Store.count
